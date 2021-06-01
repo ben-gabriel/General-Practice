e22cd7c6b4327a473193b3e43727bc34dad94aaa
Sergio Gabriel Benitez <ben.gabriel.w@gmail.com>
2021-06-01 06:34:18 -0300

    refactor async functions

--- JS-basics-general-practice/Fetch-Forms-practice/script.js
+++ JS-basics-general-practice/Fetch-Forms-practice/script.js
@@ -1,20 +1,25 @@
-console.log('first message');
-console.log(
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-);
-console.log('second message');
+//https://pokeapi.co/api/v2/pokemon/ditto
 
-async function fetcher(element) {
-    let myvar = await fetch('https://pokeapi.co/api/v2/pokemon/charizard');
-    let myjson = await myvar.json();
-    console.log(myjson);
+// async function fetcher(element) {
+//     let myvar = await fetch('https://pokeapi.co/api/v2/pokemon/raichu');
+//     let myjson = await myvar.json();
+//     console.log(myjson);
     
-    console.log(myjson.sprites.other['official-artwork'].front_default);
-    element.src = myjson.sprites.other['official-artwork'].front_default;
+//     console.log(myjson.sprites.other['official-artwork'].front_default);
+//     element.src = myjson.sprites.other['official-artwork'].front_default;
+// }
+
+async function fetcher(name){
+    console.log(name+' inside fetcher');
+    return ((await fetch('https://pokeapi.co/api/v2/pokemon/'+name)).json());
+    //returns the promise of a json object given that is an async function
 }
 
-
-
+async function addImage(name, newElement){
+    console.log(name+' inside add image');
+    let myjson = await fetcher(name);
+    newElement.src = myjson.sprites.other['official-artwork'].front_default;
+}
 
 const myDiv = document.getElementById('myDiv');
 
@@ -22,7 +27,7 @@ let newE = document.createElement('img');
 
 myDiv.appendChild(newE);
 
-fetcher(newE);
+addImage('pikachu',newE);
 
 //newE.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png"
 //object. sprites. other. official-artwork. front_default
