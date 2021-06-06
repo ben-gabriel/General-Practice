4fab73e4ddd3328629f024ae0a8772f39bc193e0
sergio benitez <ben.gabriel.w@gmail.com>
2021-06-06 20:58:54 -0300

    add css transition

---
+++ CSS-basics-general-practice/Pokeball/script.js
@@ -0,0 +1,20 @@
+
+let pokeball1 = document.getElementById('pokeballBody1');
+
+let degrees = 0;
+
+// setInterval(()=>{
+
+//     pokeball1.style.setProperty('--degrees', degrees+'deg');
+    
+//     degrees = degrees + 0.3;
+    
+//     if(degrees > 359){
+//         degrees = 0;
+//     }
+
+//     console.log('Degrees = ', degrees);
+
+// }, 50);
+
+// pokeball.style.setProperty('--degrees', 360+'deg');
