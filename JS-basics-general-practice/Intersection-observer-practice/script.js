2a7b09d3dfc4426b3583ab2cbbb15965e0d601a7
Sergio Gabriel Benitez <ben.gabriel.w@gmail.com>
2021-07-05 10:41:19 -0300

    finish first draft

--- JS-basics-general-practice/Intersection-observer-practice/script.js
+++ JS-basics-general-practice/Intersection-observer-practice/script.js
@@ -4,22 +4,28 @@ const sections = document.querySelectorAll('section');
 let options = {
     root: null,
     rootMargin: "0px",
-    threshold: 1
+    threshold: 0.5
 }
 
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach(entry =>{
         console.log(`${entry.target.id} is intersecting = ${entry.isIntersecting}`);
-        console.log(entry);
+        // console.log(entry);
 
-        if(entry.target.id === 'main' && entry.isIntersecting === true){
+        let main = (entry.target.id === 'main' && entry.isIntersecting === true);
+        let description = (entry.target.id === 'description' && entry.isIntersecting === true);
+        let end = (entry.target.id === 'end' && entry.isIntersecting === true);
+
+        if(main){
             root.style.setProperty('--a-color', 'var(--color-orange)')
         }
         
-        if(entry.target.id === 'description' && entry.isIntersecting === true){
+        if(description && !main){
             root.style.setProperty('--a-color', 'var(--color-darkblue)')
-        }else{
-            root.style.setProperty('--a-color', 'var(--color-orange)')
+        }
+       
+        if(end && !description){
+            root.style.setProperty('--a-color', 'var(--color-pink)')
         }
         
 
