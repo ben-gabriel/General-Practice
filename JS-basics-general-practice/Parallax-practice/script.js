b1a413acb231266a668ec04a332524b78f4def5d
Sergio Gabriel Benitez <ben.gabriel.w@gmail.com>
2021-07-06 12:15:39 -0300

    add basic syntax

---
+++ JS-basics-general-practice/Parallax-practice/script.js
@@ -0,0 +1,9 @@
+// The general idea is that, on scroll down, the images whill
+// start going up from the front to the back.
+// starting with the flat ones, up to the mountains
+// ending with the person victorious in the center as
+// in the *idea* picture.
+// it would be cool to add in a similar fashion to said image,
+// a sun or something in that vein.
+// the different layers might have to be in different colors.
+
