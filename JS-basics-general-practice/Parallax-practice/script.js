110a75ce19fbe7e40eeac4024be09576fe04bce4
Sergio Gabriel Benitez <ben.gabriel.w@gmail.com>
2021-07-07 07:03:48 -0300

    add resize and scroll event listeners

--- JS-basics-general-practice/Parallax-practice/script.js
+++ JS-basics-general-practice/Parallax-practice/script.js
@@ -7,3 +7,49 @@
 // a sun or something in that vein.
 // the different layers might have to be in different colors.
 
+// Get images from DOM
+const img5 = document.getElementById('img5');
+const img4 = document.getElementById('img4');
+const img3 = document.getElementById('img3');
+const img2 = document.getElementById('img2');
+const img1 = document.getElementById('img1');
+
+
+// Get window inner height, updated on resize
+let viewportHeigth = window.innerHeight;
+
+window.addEventListener('resize', ()=>{
+    viewportHeigth = window.innerHeight;
+    console.log('inner height: ', window.innerHeight);
+});
+
+
+// Calculate a percentage using rule of 3
+let pixelsScrolled;
+let percentageScrolled;
+
+document.addEventListener('scroll', ()=>{
+    
+    // Rule of three:
+    //
+    // 10(px) -> 100(%)
+    // 5(px) -> ?
+    //   ? = (5 * 100)/10 = 50(%) 
+    // 
+    // viewportHeight -> 100
+    // pixelsScrolled -> percentageScrolled
+    //   percentageScrolled = (pixelsScrolled * 100)/viewportHeight 
+    
+    pixelsScrolled = window.scrollY;
+
+    // percentageScrolled = (pixelsScrolled * 100)/viewportHeigth;
+    percentageScrolled = Math.round((pixelsScrolled * 100)/viewportHeigth);
+    
+
+    
+
+
+    console.log('pixelsScrolled: ', pixelsScrolled);
+    console.log('percentageScrolled: ', percentageScrolled);
+});
+
